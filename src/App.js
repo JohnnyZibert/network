759c70d23e27452b489d064c580d84e50dff79b8
kurva <zgirdan@mail.ru>
2022-01-11 23:19:25 +0300

    add console.log()

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import { Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 const App = (props) => {
+    console.log("123")
+
     return (
             <div className='app-wrapper'>
                 <Header />
